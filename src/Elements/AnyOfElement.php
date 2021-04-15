62d5d7b5603d30862ee2d1229b515c0e1cb4fa89
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2021-04-15 21:37:37 +0700

    AnyOfElement

--- src/Elements/AnyOfElement.php
+++ src/Elements/AnyOfElement.php
@@ -6,8 +6,15 @@ namespace CodexSoft\Transmission\Schema\Elements;
 
 class AnyOfElement extends AbstractCompositeElement
 {
+    /** @var AbstractElement[] */
     protected array $variants;
 
+    /**
+     * AnyOfElement constructor.
+     *
+     * @param AbstractElement[] $variants
+     * @param string $label
+     */
     public function __construct(array $variants, string $label = '')
     {
         parent::__construct($label);
@@ -16,6 +23,27 @@ class AnyOfElement extends AbstractCompositeElement
 
     public function toOpenApiSchema(): array
     {
-        // TODO: Implement toOpenApiSchema() method.
+        $oaVariants = [];
+        foreach ($this->variants as $variant) {
+            $oaVariants[] = $variant->toOpenApiSchema();
+        }
+
+        $data = [
+            'anyOf' => $oaVariants,
+            'description' => $this->label,
+            'required' => $this->isRequired,
+            'nullable' => $this->isNullable,
+            'deprecated' => $this->isDeprecated,
+        ];
+
+        //if ($this->example !== self::UNDEFINED) {
+        //    $data['example'] = $this->example;
+        //}
+
+        //if ($this->hasDefaultValue()) {
+        //    $data['default'] = $this->defaultValue;
+        //}
+
+        return $data;
     }
 }
