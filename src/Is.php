0371696b946a2f64cb1d9182f566f47837012db9
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2021-04-23 19:41:52 +0700

    cleanup

--- src/Is.php
+++ src/Is.php
@@ -3,16 +3,9 @@
 
 namespace CodexSoft\Transmission\Schema;
 
-use CodexSoft\Transmission\Schema\Elements\AbstractElementBuilderInterface;
-use CodexSoft\Transmission\Schema\Elements\StringElement;
-
 /**
  * Alias
  */
 class Is extends Accept
 {
-    public static function test(): AbstractElementBuilderInterface
-    {
-        return new StringElement();
-    }
 }
